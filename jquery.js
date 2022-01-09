af1e4c43bed0e52e77ab059e6d2272b4e6d7dc56
fiona <fibsy79@yahoo.com>
2022-01-09 14:29:13 +0200

    new additions

---
+++ jquery.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+
+    //A function which contains hiding/showing.
+    $("#butt").click(function () {
+        $("#more").toggle();
+
+    });
+    $("#headingimg").mouseenter(function () {
+        $("#heading1").animate({
+            padding: "-=100px"
+        });
+        prompt
+    });
+});
